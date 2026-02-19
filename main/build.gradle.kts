c4985cb4a7f58a2bd9cd87abcee75f9fb0d7e03c
semyanovame <semyanovame@306D-13>
2026-02-19 13:25:48 +0700

    1 теперь ок

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -40,10 +40,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.activity:activity-ktx:1.9.2")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.activity:activity-ktx:1.12.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.lifecycle.livedata.ktx)
--- main/src/main/java/ci/nsu/moble/main/ui/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/ui/main/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello33 $name!",
         modifier = modifier
     )
 }
